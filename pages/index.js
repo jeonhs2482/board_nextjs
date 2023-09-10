26c745792d7fd7e8f66f587001b3674d549c3ad3
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2023-09-10 23:19:32 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import { Mongoclient } from "mongodb";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://jeon:hyun8947@cluster0.vhejpe8.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
